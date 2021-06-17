5f3ff4f28341c6753b79d66fe34a90bebd877efb
JReesW <j.rees.w@gmail.com>
2021-06-17 15:40:12 +0200

    #10 Saving and loading ratings using CSV
    Created functions to save/load ratings for people and movies. If the files are absent then they will be created. Entries are saved by their id and their scores.

--- data.py
+++ data.py
@@ -1,5 +1,6 @@
 from imdb import IMDb
 from typing import List
+import csv
 
 ia = IMDb()
 
@@ -10,11 +11,11 @@ class Entry:
 
 
 class Movie(Entry):
-    def __init__(self, movie):
+    def __init__(self, movie, scores=(0,0)):
         self.id = movie.movieID
         self.title = movie.get('title')
         self.cast = movie.get('cast')
-        self.scores = 0.0
+        self.scores = scores
 
     def basic_info(self):
         return {
@@ -25,11 +26,11 @@ class Movie(Entry):
 
 
 class Person(Entry):
-    def __init__(self, person):
+    def __init__(self, person, scores=(0,[0])):
         self.id = person.personID
         self.name = person['name']
         #self.biography = person['biography']
-        self.scores = 0.0
+        self.scores = scores
 
     def basic_info(self):
         return {
@@ -53,3 +54,41 @@ def search_movie(query: str, amount: int) -> List[object]:
 
 def search_person(query: str, amount: int) -> List[object]:
     return [Person(p) for p in list(ia.search_person(query))[0:amount]]
+
+
+def save_person_rating(id_, rating, results):
+    rows = load_person_ratings()
+    rows[id_] = (rating, results)
+    with open('people.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        for row in rows:
+            writer.writerow([row, rows[row][0], *rows[row][1]])
+
+
+def load_person_ratings():
+    try:
+        with open("people.csv", 'r', newline='') as csvfile:
+            reader = csv.reader(csvfile)
+            return {k: (r, t) for k, r, *t in reader}
+    except FileNotFoundError:
+        _ = open("people.csv", 'x', newline='')
+        return {}
+
+
+def save_movie_rating(id_, rating, prediction):
+    rows = load_movie_ratings()
+    rows[id_] = (rating, prediction)
+    with open('movies.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        for row in rows:
+            writer.writerow([row, rows[row][0], rows[row][1]])
+
+
+def load_movie_ratings():
+    try:
+        with open("movies.csv", 'r', newline='') as csvfile:
+            reader = csv.reader(csvfile)
+            return {k: (r, p) for k, r, p in reader}
+    except FileNotFoundError:
+        _ = open("movies.csv", 'x', newline='')
+        return {}
